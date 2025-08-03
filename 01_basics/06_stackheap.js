2378a8ec1ea4c0e134553e1cd3ef2c6fe9d8531f
Farihaz2812 <farihazaheer2812@gmail.com>
2025-08-03 15:31:39 +0000

    stackheap

---
+++ 01_basics/06_stackheap.js
@@ -0,0 +1,20 @@
+//Stack (primitive),Heap(Non-Primitive)
+let myYoutubename ="farihadotcom"
+
+let anothername = myYoutubename
+anothername ="farihazzz"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne = {
+    email: "user@google.com"
+    upi:"user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email ="fariha@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
